2e3a30dbf695858eaa0deb94d53ac11996772601
Ahmed Bahrawy <ahmed.bahrawy@hu.edu.eg>
2026-09-03 23:42:35 +0300

    Refine couple name font and venue address

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ function formatCalendarDate(date) {
 document.querySelector("#calendar-link").href = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(
   "زفاف د. أحمد بحراوي ود. إلهام عبد العال",
 )}&dates=${formatCalendarDate(weddingDate)}/${formatCalendarDate(weddingEndDate)}&location=${encodeURIComponent(
-  "Chateau M",
+  "عرابي فيلا 700 Chateau M",
 )}&details=${encodeURIComponent(
-  "يتشرف الحاج بحراوي منصور والمهندس عبدالعال علي بدعوتكم لحضور حفل زفاف د. أحمد بحراوي ود. إلهام عبد العال في Chateau M. الخريطة: https://maps.app.goo.gl/3n4PE4BDrfPfru816",
+  "يتشرف الحاج بحراوي منصور والمهندس عبدالعال علي بدعوتكم لحضور حفل زفاف د. أحمد بحراوي ود. إلهام عبد العال في عرابي فيلا 700 Chateau M. الخريطه: https://maps.app.goo.gl/3n4PE4BDrfPfru816",
 )}`;
 
 updateCountdown();
